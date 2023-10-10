9d3a7271c796500d50d62a15bf6adcc7cc95c282
Moin <ourmoin@gmail.com>
2023-10-10 10:09:55 +0000

    update

--- components/signupBtn.js
+++ components/signupBtn.js
@@ -2,14 +2,14 @@ const signupBtnData = [
   {}
 ];
 
-// function signupBtnTemplate(signupBtn) {
-//     return `
-//     <div class="hero-btn">
-//         <a class="btn btn-warning" href="https://www.areaphonebook.com/signup"><i
-//                 class="fas fa-user-plus"></i> আপনার তথ্য যোগ করুন</a>
-//     </div>
-//   `;
-// }
+function signupBtnTemplate(signupBtn) {
+    return `
+    <div class="hero-btn">
+        <a class="btn btn-warning" href="https://forms.gle/qAUQk6n1unWJuqxx8"><i
+                class="fas fa-user-plus"></i> আপনার তথ্য যোগ করুন</a>
+    </div>
+  `;
+}
 
 document.getElementById("signupBtn").innerHTML = `
   ${signupBtnData.map(signupBtnTemplate).join("")}  
